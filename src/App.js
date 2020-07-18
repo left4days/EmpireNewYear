88d88afca3063bc2a57179c6fd53172b99ebcee1
Kobernik.EV <Kobernik.EV@parma.ru>
2020-07-18 17:56:42 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,111 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import firebase from 'firebase';
+import axios from 'axios';
+import get from 'lodash/get';
+
+import { Header } from 'widgets/Header';
+import { Modal } from 'ui/Modal';
+import DevelopTower from './DevelopTower';
+import routes from './routes';
+import { signOutUser } from 'widgets/Auth/firebase-configuration';
+
+import style from './style.scss';
+
+const config = {
+    apiKey: "AIzaSyA3YXk8w0t2E58C1gCPUBi1dOn_M05ARk8",
+    authDomain: "sg-challenge.firebaseapp.com",
+    databaseURL: "https://sg-challenge.firebaseio.com",
+    projectId: "sg-challenge",
+    storageBucket: "sg-challenge.appspot.com",
+    messagingSenderId: "698626447936",
+    appId: "1:698626447936:web:f957f1cff204739d8e5da5",
+    measurementId: "G-Z2SLXT6N79"
+};
+
+firebase.initializeApp(config);
+
+class App extends Component {
+    state = { modal: null, user: 'loading', actionState: 'ACTIVE', mainWinnerData: {}, product_link: false };
+
+    componentWillMount() {
+        firebase.auth().onAuthStateChanged(res => {
+            const uid = get(res, 'uid', '');
+            if (uid) {
+                axios.get(`/api/v1/user/${uid}`).then(resData => {
+                    const user = { ...res, userData: resData.data };
+                    this.setState({ user, modal: null });
+                });
+            } else {
+                this.setState({ user: false });
+            }
+        });
+
+        axios.get('/api/v1/appState/state').then(res => {
+            const { state, mainWinnerData, product_link } = get(res, 'data.data', {});
+            this.setState({ actionState: state, mainWinnerData, product_link });
+        });
+    }
+
+    handleModal = type => {
+        this.setState({ modal: type });
+    };
+
+    handleModalClose = () => {
+        this.setState({ modal: null });
+    };
+
+    signOutUserAction = () => {
+        signOutUser();
+        this.setState({ user: false });
+    };
+
+    formRef = ref => (this.form = ref);
+
+    onDevValidChange = () => {
+        window.localStorage.setItem('devMode', 1);
+        this.setState({ devConfirmed: true });
+    };
+
+    render() {
+        const { modal, user, actionState, mainWinnerData, product_link } = this.state;
+        const isDevMode = !!window.localStorage.getItem('devMode');
+        if (actionState === 'DEV' && !isDevMode) {
+            return <DevelopTower onValidChange={this.onDevValidChange} />;
+        }
+
+        return (
+            <Router>
+                <Header handleModal={this.handleModal} signOutUser={this.signOutUserAction} user={user} />
+                <div className="app">
+                    {routes.map(route => {
+                        const { path, exact, Component } = route;
+                        return (
+                            <Route
+                                key={path}
+                                path={path}
+                                exact={exact}
+                                component={() => (
+                                    <Component
+                                        user={user}
+                                        actionState={actionState}
+                                        product_link={product_link}
+                                        mainWinnerData={mainWinnerData}
+                                        handleModal={this.handleModal}
+                                    />
+                                )}
+                            />
+                        );
+                    })}
+                </div>
+                <div className="footer">
+                    <Link to="/policy">Политика конфиденциальности</Link>
+                </div>
+                <Modal modal={modal} handleModal={this.handleModal} onClose={this.handleModalClose} />
+            </Router>
+        );
+    }
+}
+
+export default App;
