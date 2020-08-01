25ef27fbf6c707e800688ce23d78fbec6e6bae76
Kobernik.EV <Kobernik.EV@parma.ru>
2020-08-01 23:11:29 +0200

    1st iter

--- src/widgets/Light/index.js
+++ src/widgets/Light/index.js
@@ -1,14 +1,18 @@
 import React from "react";
 import im1 from "statics/light/1.png";
 import im2 from "statics/light/2.png";
+import im3 from "statics/light/3.png";
+import im4 from "statics/light/4.png";
 import "./style.scss";
 
 const Light = () => {
   return (
     <div className="light__wrapper">
-      <div className="light">
+      <div className="light">s
         <img src={im1} className="light_1" alt="" />
         <img src={im2} className="light_2" alt="" />
+        <img src={im3} className="light_3" alt="" />
+        <img src={im4} className="light_4" alt="" />
       </div>
     </div>
   );
