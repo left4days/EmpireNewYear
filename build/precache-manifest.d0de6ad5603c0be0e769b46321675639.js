8485b7448cec0202d4b1c669534469f9110b54cf
Kobernik.EV <Kobernik.EV@parma.ru>
2020-11-29 17:34:31 +0100

    added build

---
+++ build/precache-manifest.d0de6ad5603c0be0e769b46321675639.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f985c794f7886f6332a8e216f5f50ba",
+    "url": "/static/media/prize1.5f985c79.png"
+  },
+  {
+    "revision": "a59fe706e0416ad9d2e2",
+    "url": "/static/js/main.b9a72448.chunk.js"
+  },
+  {
+    "revision": "b2ee61393bc5e368b30f5bb2ddccb5f6",
+    "url": "/static/media/image_bacground_futer.b2ee6139.png"
+  },
+  {
+    "revision": "2c17d653723ad1dfdfd35b00ee3e173d",
+    "url": "/static/media/logo.2c17d653.svg"
+  },
+  {
+    "revision": "54aa4700b55d867c08abf6a0575ec3f1",
+    "url": "/static/media/buy3.54aa4700.svg"
+  },
+  {
+    "revision": "8b2f7097615608d0ec64bcde99b42e11",
+    "url": "/static/media/buy4.8b2f7097.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "bb5c66c59cb066a7a55026577f0a5e2f",
+    "url": "/static/media/prize2.bb5c66c5.png"
+  },
+  {
+    "revision": "19696edd853ea54362f5f4ed2f028c0e",
+    "url": "/static/media/prize3.19696edd.png"
+  },
+  {
+    "revision": "93ef6a2aea90a67c6f6dc4a0bd43d88d",
+    "url": "/static/media/image_bacground_header.93ef6a2a.png"
+  },
+  {
+    "revision": "f8ae3e2bc2f37a87c1871ced94290d31",
+    "url": "/static/media/drakon.f8ae3e2b.png"
+  },
+  {
+    "revision": "766bfdec2eeee9d18d8d",
+    "url": "/static/js/2.db98c329.chunk.js"
+  },
+  {
+    "revision": "d97fd44b26a886db087cd5ffa7db94a6",
+    "url": "/index.html"
+  }
+];
